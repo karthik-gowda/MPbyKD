cc61c7508c50188bc74e24f069f397e88bb6a89a
Karthik <karthikbt22@gmail.com>
2022-07-01 13:09:41 +0530

    New upgrade for app download

--- script.js
+++ script.js
@@ -1,42 +1,51 @@
 const menuToggle = document.querySelector(".menuToggle");
 const navigation = document.querySelector(".navigation");
 menuToggle.onclick = function () {
-    menuToggle.classList.toggle("active");
-    navigation.classList.toggle("active");
+  menuToggle.classList.toggle("active");
+  navigation.classList.toggle("active");
 };
 
 window.addEventListener("scroll", function () {
-    const header = document.querySelector("header");
-    header.classList.toggle("sticky", window.scrollY > 0);
+  const header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
 });
 
 function toggleMenu() {
-    menuToggle.classList.remove("active");
-    navigation.classList.remove("active");
+  menuToggle.classList.remove("active");
+  navigation.classList.remove("active");
 }
 var mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () {
-    scrollFunction();
+  scrollFunction();
 };
 
 function scrollFunction() {
-    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-        mybutton.style.display = "block";
-    } else {
-        mybutton.style.display = "none";
-    }
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    mybutton.style.display = "block";
+    $("#popup").css("background", "#afafaf");
+  } else {
+    mybutton.style.display = "none";
+    $("#popup").css("background", "white");
+  }
 }
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
 
 //Preloading gif till page loads
 var loader = document.getElementById("preloader");
 window.addEventListener("load", function () {
-    loader.style.display = "none";
+  loader.style.display = "none";
+  this.setTimeout(function () {
+    $("#popup").css("bottom", "50px");
+  }, 2000);
+});
+
+$("#close_pop_img").on("click", function () {
+  $("#popup").css("bottom", "-150px");
 });
